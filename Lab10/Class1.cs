bb5e9d1d38a7e11cfd8dc0ebe63318c5c4a3ca86
Zakhar Galanin <zakhargalanin@gmail.com>
2022-07-02 23:24:37 -0700

    Add project files.

---
+++ Lab10/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    internal class Angle
+    {
+        double grad;
+        double minute;
+        double second;
+
+        public double Grad
+        { get { return grad; } set { grad = value; } }
+        public double Second
+        { get { return minute; } set { minute = value; } }
+        public double Minute
+        { get { return minute; } set { minute = value; } }
+
+        public Angle(double grad, double minute, double second)
+        {
+            this.grad = grad;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public double ToRadian()
+        {
+            return (second/ (60*180) + minute /60 + grad) / (180 / Math.PI);
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grad = Convert.ToDouble(Console.ReadLine());
+            double minute = Convert.ToDouble(Console.ReadLine());
+            double second = Convert.ToDouble(Console.ReadLine());
+
+            Angle angle = new Angle(grad, minute, second);
+
+            double radian = angle.ToRadian();
+            Console.WriteLine("Угол в радианах - {0}", radian);
+            Console.WriteLine("{0} градусов {1} минут {2} секунд", grad, minute, second);
+            Console.ReadKey();
+        }
+    }
+}
